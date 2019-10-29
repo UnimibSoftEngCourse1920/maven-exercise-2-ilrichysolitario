c65fdbc0617af8caedde36a39536319d6a9f4c53
Riccardo Maria Frighi <r.frighi@campus.unimib.it>
2019-10-29 17:31:08 +0100

    Maven build executed

--- com.vogella.maven.simple/src/com/vogella/maven/simple/Main.java
+++ com.vogella.maven.simple/src/com/vogella/maven/simple/Main.java
@@ -3,7 +3,7 @@ package com.vogella.maven.simple;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Maven!!");
+		System.out.println("Hello Maven!");
 
 	}
 
